39ea9a6b22c3ad45c3ac09a5d645c8a6bbbd3593
gokulmanikandan35 <gokulmanikandan230@gmail.com>
2024-12-16 22:41:12 +0530

    conflict correction

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-<<<<<<< HEAD
 import LoginPage from "./components/Profile/LoginPage.jsx";
 import Profile from "./components/Profile/Profiles.jsx";
-import { Route, Routes } from "react-router-dom";
 import Product from "./components/Product/Product.jsx";
-=======
+
 import TShirtsPage from "./pages/Western/TShirtsPage";
 import SkirtsPage from "./pages/Western/SkirtsPage";
 import JeansPage from "./pages/Western/JeansPage";
->>>>>>> db7d36170960013edfe2d512ab247edc08f2e8fc
 import "./App.css";
 
 function App() {
   return (
-<<<<<<< HEAD
+
     <div className="App">
       <Header />
 
@@ -25,6 +22,9 @@ function App() {
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Product" element={<Product />} />
         <Route path="/Loginpage" element={<LoginPage />} />
+        <Route path="/women-western/tops" element={<TShirtsPage />} />
+        <Route path="/women-western/dresses" element={<SkirtsPage />} />
+        <Route path="/women-western/jeans" element={<JeansPage />} />
       </Routes>
 
       {/* Header */}
@@ -37,27 +37,7 @@ function App() {
         <p>© 2024 Yazh Fashion. All rights reserved.</p>
       </footer>
     </div>
-=======
-    <Router>
-      <div className="App">
-        {/* Header */}
-        <Header />
 
-        {/* Main Content with Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/women-western/tops" element={<TShirtsPage />} />
-          <Route path="/women-western/dresses" element={<SkirtsPage />} />
-          <Route path="/women-western/jeans" element={<JeansPage />} />
-        </Routes>
-
-        {/* Footer */}
-        <footer className="footer">
-          <p>© 2024 Yazh Fashion. All rights reserved.</p>
-        </footer>
-      </div>
-    </Router>
->>>>>>> db7d36170960013edfe2d512ab247edc08f2e8fc
   );
 }
 
